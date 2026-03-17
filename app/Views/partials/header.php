e99792a3f1f5138099790962d6002688d17ef60f
AndreLPDZ <lpdzandre@gmail.com>
2026-03-17 11:11:06 -0300

    Create header.php

---
+++ app/Views/partials/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>André LPDZ - Reformulação</title>
+    <link rel="stylesheet" href="/v2/public/assets/css/style.css">
+</head>
+<body>
+<nav>
+    <a href="/v2/">Dev</a> | <a href="/v2/app">Games</a> | 
+    <a href="/v2/ministerio">Ministério</a> | <a href="/v2/academy">Academy</a> | 
+    <a href="/v2/links">Links</a>
+</nav>
+<hr>
