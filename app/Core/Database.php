a8cf90ac5884bab6402864abd352f3397cf8aae8
AndreLPDZ <lpdzandre@gmail.com>
2026-03-17 10:57:33 -0300

    Create Database.php
    conexao

---
+++ app/Core/Database.php
@@ -0,0 +1,29 @@
+<?php
+namespace Config;
+
+use PDO;
+use PDOException;
+
+class Database {
+    private $host = "localhost";
+    private $db_name = "seu_banco";
+    private $username = "seu_usuario";
+    private $password = "sua_senha";
+    public $conn;
+
+    public function getConnection() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO(
+                "mysql:host=" . $this->host . ";dbname=" . $this->db_name,
+                $this->username,
+                $this->password,
+                [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"]
+            );
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $exception) {
+            echo "Erro de conexão: " . $exception->getMessage();
+        }
+        return $this->conn;
+    }
+}
